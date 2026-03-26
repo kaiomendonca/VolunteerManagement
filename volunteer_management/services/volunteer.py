ea757738b0302939d3605cf9f22eab8d84ba5c7a
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-03-26 10:35:45 -0300

    fix: handle duplicate email error with proper HTTP response and IntegrityError

--- volunteer_management/services/volunteer.py
+++ volunteer_management/services/volunteer.py
@@ -1,13 +1,24 @@
 from sqlalchemy.orm import Session
 from volunteer_management.models.volunteer import Volunteer
 from volunteer_management.schemas.volunteer import VolunteerCreate
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException
 
 def create_volunteer(db: Session, volunteer_in: VolunteerCreate):
     db_volunteer = Volunteer(**volunteer_in.model_dump())
     db.add(db_volunteer)
-    db.commit()
-    db.refresh(db_volunteer)
-    return db_volunteer    
+    
+    try:
+        db.commit()
+        db.refresh(db_volunteer)
+        return db_volunteer
+    
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code = 400,
+            detail="Email already registered."
+        )     
 
 
 def list_volunteers(db: Session,
