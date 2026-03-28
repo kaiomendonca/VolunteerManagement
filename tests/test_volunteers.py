4bc4ead8632d4779f09a939a475cb77c7a4a7c8e
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-03-28 11:39:43 -0300

    test(volunteer): add tests for valid creation and duplicate email validation

---
+++ tests/test_volunteers.py
@@ -0,0 +1,37 @@
+from volunteer_management.models.volunteer import AvailabilityEnum, CargoEnum
+
+
+def test_create_valid_volunteer(client):
+    """Should create volunteer with valid data"""
+
+    payload = {
+        "name": "João Silva",
+        "email": "joao@email.com",
+        "phone": "21999999999",
+        "desired_position": "backend",
+        "availability": "weekend" 
+    }
+
+    response = client.post("/volunteers", json=payload)
+
+    assert response.status_code == 200
+    assert response.json()["email"] == payload["email"]
+
+
+def test_do_not_allow_duplicate_email(client):
+    """Should not allow registering with duplicate email"""
+
+    payload = {
+        "name": "João Silva",
+        "email": "joao@email.com",
+        "phone": "21999999999",
+        "desired_position": "backend",
+        "availability": "weekend"
+    }
+
+    response_first = client.post("/volunteers", json=payload)
+    assert response_first.status_code == 200
+
+    response_second = client.post("/volunteers", json=payload)
+    assert response_second.status_code == 400
+    assert response_second.json()["detail"] == "Email already registered."
